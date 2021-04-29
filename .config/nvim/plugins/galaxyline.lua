29789ce7342a1f60e3d93e4f40f1bf07da063b48
kghugo <hugosum.dev@protonmail.com>
2021-04-29 23:25:08 +0800

    Use evil lin

--- .config/nvim/plugins/galaxyline.lua
+++ .config/nvim/plugins/galaxyline.lua
@@ -1,7 +1,8 @@
 local function init(paq)
   paq{'glepnir/galaxyline.nvim', branch='main'}
   --require('spaceline')
-  require('plugins.siduck76')
+  --require('plugins.siduck76')
+  require('plugins.custom-statusbar')
 end
 
 return {
--- .config/nvim/plugins/indent-blankline.lua
+++ .config/nvim/plugins/indent-blankline.lua
@@ -2,6 +2,7 @@ local function init(paq)
   paq{'lukas-reineke/indent-blankline.nvim', branch='lua'}
 
 vim.g.indent_blankline_use_treesitter = true
+vim.g.indent_blankline_char = '│'
 
 end
 
