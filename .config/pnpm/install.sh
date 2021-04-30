337a230df7cb06bbc32931581f4ec47d86a2669e
kghugo <hugosum.dev@protonmail.com>
2021-04-30 14:06:06 +0800

    Add install shell for installing global packages

---
+++ .config/pnpm/install.sh
@@ -0,0 +1,18 @@
+#Install pnpm
+npm install -g pnpm
+#Install all global packages
+pnpm install -g autocannon
+#Linter and formatter
+pnpm install -g prettier
+pnpm install -g eslint eslint_d
+#Language server
+pnpm install -g vscode-css-languageserver-bin
+pnpm install -g dockerfile-language-server-nodejs
+pnpm install -g graphql-language-service-cli
+pnpm install -g vscode-html-languageserver-bin
+pnpm install -g vscode-json-languageserver
+pnpm install -g purescript-language-server
+pnpm install -g svelte-language-server
+pnpm install -g typescript typescript-language-server
+pnpm install -g vim-language-server
+pnpm install -g vls
---
+++ .config/pnpm/readme.md
@@ -0,0 +1,11 @@
+# Notice
+
+This directory is not created by pnpm automatically. This is manually
+updated and created for porting packages across multiple computers
+easily.
+
+## How to use?
+
+Run the `install.sh` to start the installation process.
+
+
