11370a59381dc453f35692000645545614fc4f8c
kghugo <hugosum.dev@protonmail.com>
2021-05-05 17:33:31 +0800

    Use hop instead of easy-motion

--- .config/nvim/plugins/easy-motion.lua
+++ .config/nvim/plugins/easy-motion.lua
@@ -1,8 +1,11 @@
 local function init(paq)
-		paq{'easymotion/vim-easymotion'}
-		vim.cmd [[ map <Leader> <Plug>(easymotion-prefix) ]]
-		vim.cmd [[ nmap <Leader><Leader> <Plug>(easymotion-overwin-w) ]]
-		vim.g.EasyMotion_do_mapping = 0
+		paq{'phaazon/hop.nvim'}
+		require'hop'.setup{}
+		vim.cmd [[ nmap <Leader><Leader> <cmd>HopWord<cr>]]
+		-- paq{'easymotion/vim-easymotion'}
+		-- vim.cmd [[ map <Leader> <Plug>(easymotion-prefix) ]]
+		-- vim.cmd [[ nmap <Leader><Leader> <Plug>(easymotion-overwin-w) ]]
+		-- vim.g.EasyMotion_do_mapping = 0
 end
 
 return {
