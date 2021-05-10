f71c2cc1d15d18f9dbb17c73bd46af48c0bc5304
kghugo <hugosum.dev@protonmail.com>
2021-05-10 21:29:18 +0800

    Add mapping for toggle comment in Visual mode

--- .config/nvim/plugins/nerd-commenter.lua
+++ .config/nvim/plugins/nerd-commenter.lua
@@ -3,12 +3,12 @@ local function init(paq)
 		require('nvim_comment').setup({ create_mappings = false })
 		-- vim.cmd([[ nnoremap <expr> <leader>/ (v:count ? "<CMD>CommentToggle<CR>" : ':set operatorfunc=CommentOperator<CR>g@') ]])
 		vim.cmd([[ nnoremap <expr> <leader>/ "<CMD>CommentToggle<CR>" ]])
+		vim.cmd([[ vnoremap <expr> <leader>/ "<CMD>CommentToggle<CR>" ]])
 		vim.cmd([[ command! -range Cm <line1>,<line2>CommentToggle ]])
 end
 
 return {
 	init = init
 }
-
 
 -- vim.cmd([[ nnoremap <expr> <leader>/ v:count . "<CMD>CommentToggle<CR>" ]])
