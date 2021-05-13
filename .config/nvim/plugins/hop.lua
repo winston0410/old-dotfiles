eb0506212240dca3b759ede75d556823ba6e57a3
kghugo <hugosum.dev@protonmail.com>
2021-05-13 17:23:32 +0800

    Fix hop commands

--- .config/nvim/plugins/hop.lua
+++ .config/nvim/plugins/hop.lua
@@ -1,15 +1,11 @@
 local function init(paq)
-  paq{'phaazon/hop.nvim'}
-  require'hop'.setup{}
-  vim.cmd [[ nmap <Leader><Leader> <cmd>HopChar1<cr>]]
-  vim.cmd [[ nmap <Leader>w <cmd>HopWord<cr>]]
-  -- paq{'easymotion/vim-easymotion'}
-  -- vim.cmd [[ map <Leader> <Plug>(easymotion-prefix) ]]
-  -- vim.cmd [[ nmap <Leader><Leader> <Plug>(easymotion-overwin-w) ]]
-  -- vim.g.EasyMotion_do_mapping = 0
+		paq{'phaazon/hop.nvim'}
+		require'hop'.setup{}
+		vim.cmd [[ nmap <Leader><Leader> <cmd>HopChar1<cr>]]
+		vim.cmd [[ nmap <Leader>w <cmd>HopWord<cr>]]
 end
 
 return {
-  init = init
+	init = init
 }
 
