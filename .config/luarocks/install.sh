e49f85d4742ea7d8d5d180374f4b153d60366f1b
kghugo <hugosum.dev@protonmail.com>
2021-05-14 17:52:39 +0800

    Download multiple packages from lua

--- .config/luarocks/install.sh
+++ .config/luarocks/install.sh
@@ -1,7 +1,14 @@
-PACKAGES=$(cat <<- END
-penlight
-luaformatter
-END
-)
+# packages=$(cat <<- end
+# penlight
+# luaformatter
+# luapak
+# luafilesystem
+# end
+# )
 
-luarocks install --server=https://luarocks.org/dev $PACKAGES
+# luarocks install --server=https://luarocks.org/dev $packages
+luarocks install --server=https://luarocks.org/dev luaformatter
+luarocks install --server=https://luarocks.org/dev luapak
+luarocks install --server=https://luarocks.org/dev luafilesystem
+
+
