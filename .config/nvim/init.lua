997a9b31f7a99fa3a6a84622dd8e0b6dd11ebc28
kghugo <hugosum.dev@protonmail.com>
2021-05-15 23:57:07 +0800

    Add numb for peeking lines on command

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -10,7 +10,9 @@ paq{'savq/paq-nvim', opt=true}     -- Let Paq manage itself
 
 paq{'wbthomason/packer.nvim'}
 require('plugins.lspconfig').init(paq)
-require('plugins.smart_number').setup(paq)
+require('plugins.smart_number').setup()
+require('plugins.extmarks').setup()
+require('plugins.yank_highlight').setup()
 paq{'kyazdani42/nvim-web-devicons'}
 require('plugins.completion').init(paq)
 require('plugins.trouble').init(paq)
@@ -24,6 +26,7 @@ require('plugins.telescope').init(paq)
 require('plugins.galaxyline').init(paq)
 require('plugins.formatter').init(paq)
 require('plugins.colorizer').init(paq)
+require('plugins.numb').init(paq)
 
 vim.cmd [[
 if !exists('g:syntax_on')
