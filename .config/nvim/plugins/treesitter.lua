34e8517483e94f166e2365f341ce1d0ca6d819f8
kghugo <hugosum.dev@protonmail.com>
2021-05-21 15:08:29 +0800

    Add TSInstall command

--- .config/nvim/plugins/treesitter.lua
+++ .config/nvim/plugins/treesitter.lua
@@ -1,23 +1,17 @@
---https://github.com/nvim-treesitter/nvim-treesitter-textobjects
+-- https://github.com/nvim-treesitter/nvim-treesitter-textobjects
+-- TSInstall dart yaml vue tsx typescript toml teal svelte rust lua json javascript html go bash
 local function init(paq)
-  paq{'nvim-treesitter/nvim-treesitter'}
-  local text_object = require('plugins.text-object')
-  text_object.init(paq)
-  local treesitter = require('nvim-treesitter.configs')
-  treesitter.setup {
-	highlight = {
-	  enable = true
-	}
-  }
+    paq {'nvim-treesitter/nvim-treesitter'}
+    local text_object = require('plugins.text-object')
+    text_object.init(paq)
+    local treesitter = require('nvim-treesitter.configs')
+    treesitter.setup {highlight = {enable = true}}
 end
 
-return {
-  init = init
-}
+return {init = init}
 
-
-	--text-object = {
-	  --select = text_object.select,
-	  --swap = text_object.swap,
-	  --move = text_object.move
-	--},
+-- text-object = {
+-- select = text_object.select,
+-- swap = text_object.swap,
+-- move = text_object.move
+-- },
