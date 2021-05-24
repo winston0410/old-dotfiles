5fbec5a4705a49c0e0689fb21bd3427100c93dbe
kghugo <hugosum.dev@protonmail.com>
2021-05-24 09:58:00 +0800

    Change tab setting for yaml files

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -3,6 +3,7 @@ package.path = package.path .. ";" .. vim.fn.getenv("HOME") .. "/.config/nvim/?.
 vim.cmd [[ source $HOME/.config/nvim/keymap.vim ]]
 vim.cmd [[ source $HOME/.config/nvim/command-alias.vim ]]
 vim.cmd [[ source $HOME/.config/nvim/values.vim ]]
+vim.cmd [[ source $HOME/.config/nvim/yaml.vim ]]
 
 vim.cmd 'packadd paq-nvim'         -- Load package
 local paq = require'paq-nvim'.paq  -- Import module and bind `paq` function
@@ -11,7 +12,7 @@ paq{'savq/paq-nvim', opt=true}     -- Let Paq manage itself
 paq{'wbthomason/packer.nvim'}
 require('plugins.lspconfig').init(paq)
 require('plugins.smart_number').setup()
-require('plugins.extmarks').setup()
+-- require('plugins.extmarks').setup()
 require('plugins.yank_highlight').setup()
 paq{'kyazdani42/nvim-web-devicons'}
 require('plugins.completion').init(paq)
@@ -26,7 +27,8 @@ require('plugins.telescope').init(paq)
 require('plugins.galaxyline').init(paq)
 require('plugins.formatter').init(paq)
 require('plugins.colorizer').init(paq)
-require('plugins.numb').init(paq)
+-- require('plugins.numb').init(paq)
+require('plugins.range-highlight').init(paq)
 
 vim.cmd [[
 if !exists('g:syntax_on')
