b14ddb0293a725fb4a168c360004740165f700bb
kghugo <hugosum.dev@protonmail.com>
2021-05-28 11:28:10 +0800

    Add snippets expanding mapping

--- .config/nvim/plugins/completion.lua
+++ .config/nvim/plugins/completion.lua
@@ -1,35 +1,36 @@
 local function init(paq)
-  vim.o.completeopt = "menuone,noselect"
+    vim.o.completeopt = "menuone,noselect"
 
-  paq{'hrsh7th/vim-vsnip'}
-  paq{"rafamadriz/friendly-snippets"}
-  paq{'hrsh7th/nvim-compe'}
+    paq {'hrsh7th/vim-vsnip'}
+    paq {"rafamadriz/friendly-snippets"}
+    paq {'hrsh7th/nvim-compe'}
 
-  require'compe'.setup {
-	enabled = true;
-	autocomplete = true;
-	debug = false;
-	min_length = 1;
-	preselect = 'enable';
-	throttle_time = 80;
-	source_timeout = 200;
-	incomplete_delay = 400;
-	max_abbr_width = 100;
-	max_kind_width = 100;
-	max_menu_width = 100;
-	documentation = true;
+    require'compe'.setup {
+        enabled = true,
+        autocomplete = true,
+        debug = false,
+        min_length = 1,
+        preselect = 'enable',
+        throttle_time = 80,
+        source_timeout = 200,
+        incomplete_delay = 400,
+        max_abbr_width = 100,
+        max_kind_width = 100,
+        max_menu_width = 100,
+        documentation = true,
 
-	source = {
-	  path = true;
-	  buffer = true;
-	  calc = true;
-	  nvim_lsp = true;
-	  nvim_lua = true;
-	  vsnip = true;
-	};
-  }
+        source = {
+            path = true,
+            buffer = true,
+            calc = true,
+            nvim_lsp = true,
+            nvim_lua = true,
+            vsnip = true
+        }
+    }
+
+    vim.api.nvim_set_keymap('i', '<CR>', "compe#confirm('<CR>')",
+                            {expr = true, noremap = true, silent = true})
 end
 
-return {
-  init = init
-}
+return {init = init}
