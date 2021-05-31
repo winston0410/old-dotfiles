c1118c1f817474f62940ed9b665cb7113d9d1133
kghugo <hugosum.dev@protonmail.com>
2021-05-31 23:03:56 +0800

    Add filetype for nix

--- .config/nvim/custom-filetypes.lua
+++ .config/nvim/custom-filetypes.lua
@@ -3,6 +3,7 @@ augroup envFiletype
 autocmd!
 autocmd BufNewFile,BufRead *.env set filetype=dotenv.sh
 autocmd BufNewFile,BufRead *.tl set filetype=teal
+autocmd BufNewFile,BufRead *.nix set filetype=nix
 autocmd BufNewFile,BufRead package.json set filetype=packageJson.json
 augroup END
 ]], true)
