d08274d316da6a6fd02655dbd796c7060b589369
kghugo <hugosum.dev@protonmail.com>
2021-06-01 00:33:25 +0800

    add config for various hardware

--- .nixpkgs/darwin-configuration.nix
+++ .nixpkgs/darwin-configuration.nix
@@ -2,7 +2,37 @@
 
 {
   programs.zsh.enable = true; # default shell on catalina
-  environment.systemPackages = [ pkgs.nixfmt pkgs.alacritty pkgs.tmux pkgs.git pkgs.go pkgs.ninja pkgs.bat pkgs.fzf pkgs.fd pkgs.ripgrep];
+  environment.systemPackages = [ pkgs.nixfmt pkgs.alacritty pkgs.tmux pkgs.git pkgs.go pkgs.ninja pkgs.bat pkgs.fzf pkgs.fd pkgs.ripgrep pkgs.dart];
   system.stateVersion = 4;
   nix.useDaemon = true;
+
+  environment.variables = {
+
+  };
+
+  # Keyboard
+  system.keyboard = {
+    enableKeyMapping = true;
+    remapCapsLockToControl = true;
+  };
+
+# Dock configuration
+  system.defaults.dock = {
+	  autohide = true;
+	  mru-spaces = false;
+	  minimize-to-application = true;
+  };
+
+# Finder configuration
+  system.defaults.finder = {
+	  AppleShowAllExtensions = true;
+	  _FXShowPosixPathInTitle = true;
+	  FXEnableExtensionChangeWarning = false;
+  };
+
+# Trackpad
+  system.defaults.trackpad = {
+	  Clicking = true;
+	  TrackpadThreeFingerDrag = true;
+  };
 }
