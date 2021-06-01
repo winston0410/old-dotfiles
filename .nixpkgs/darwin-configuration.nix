6a7d83c424fee2004ca5671c352c44cb882813e3
kghugo <hugosum.dev@protonmail.com>
2021-06-01 23:50:56 +0800

    Update nix config into an object

--- .nixpkgs/darwin-configuration.nix
+++ .nixpkgs/darwin-configuration.nix
@@ -23,10 +23,10 @@
     pkgs.hadolint
     pkgs.shellcheck
     pkgs.direnv
+    pkgs.cachix
     pkgs.exa
   ];
   system.stateVersion = 4;
-  nix.useDaemon = true;
 
   environment.variables = {
     NIX_REMOTE = "daemon";
@@ -38,32 +38,32 @@
   environment.shells = [ "/run/current-system/sw/bin/zsh" ];
   environment.systemPath = [ "$HOME/go/bin" "$HOME/Library/Python/3.9/bin" ];
   environment.interactiveShellInit = ''
-            alias find="fd"
-            alias grep="rg"
-            alias vi="nvim"
-            alias vim="nvim"
-            alias vimdiff="nvim -d"
-            alias ls="exa --icons"
-            # use gcc10 instead of appleclang
-            alias gcc='gcc-11'
-            alias cc='gcc-11'
-            alias g++='g++-11'
-            alias c++='c++-11'
-            # add pip3 alias
-            alias pip="pip3.9"
-            # fzf with preview
-            alias pfzf="fzf --preview 'bat --style=numbers --color=always --line-range :500 {}'"
-            # Load tmuxp config
-            alias start="sh ~/.tmuxp/start.sh"
+                alias find="fd"
+                alias grep="rg"
+                alias vi="nvim"
+                alias vim="nvim"
+                alias vimdiff="nvim -d"
+                alias ls="exa --icons"
+                # use gcc10 instead of appleclang
+                alias gcc='gcc-11'
+                alias cc='gcc-11'
+                alias g++='g++-11'
+                alias c++='c++-11'
+                # add pip3 alias
+                alias pip="pip3.9"
+                # fzf with preview
+                alias pfzf="fzf --preview 'bat --style=numbers --color=always --line-range :500 {}'"
+                # Load tmuxp config
+                alias start="sh ~/.tmuxp/start.sh"
 
-			alias dotfiles='/run/current-system/sw/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
+    			alias dotfiles='/run/current-system/sw/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
 
-    		# Change cmake compiler
-        	export CC=$(which gcc-11)
-        	export CXX=$(which g++-11)
+        		# Change cmake compiler
+            	export CC=$(which gcc-11)
+            	export CXX=$(which g++-11)
 
-    		alias luamake=/Users/hugosum/.config/standalone/lua-language-server/3rd/luamake/luamake
-          '';
+        		alias luamake=/Users/hugosum/.config/standalone/lua-language-server/3rd/luamake/luamake
+              '';
 
   fonts = {
     enableFontDir = true;
@@ -118,12 +118,12 @@
     TrackpadThreeFingerDrag = false;
   };
 
-  # Nix config
-  nix.binaryCaches = [ "https://cache.nixos.org" ];
-  nix.trustedBinaryCaches = [ "https://cache.nixos.org" ];
-
-  nix.buildCores = 2;
-
-  nix.gc.automatic = true;
+  nix = {
+    useDaemon = true;
+    binaryCaches = [ "https://cache.nixos.org" ];
+    trustedBinaryCaches = [ "https://cache.nixos.org" ];
+    buildCores = 2;
+    gc.automatic = true;
+  };
 
 }
