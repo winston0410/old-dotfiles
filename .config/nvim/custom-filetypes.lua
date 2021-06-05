8830e19a1f2c7d87a05f1da3c9991e0de8593d8f
kghugo <hugosum.dev@protonmail.com>
2021-06-05 09:04:33 +0800

    Add filetype for sh file

--- .config/nvim/custom-filetypes.lua
+++ .config/nvim/custom-filetypes.lua
@@ -5,6 +5,8 @@ autocmd BufNewFile,BufRead *.env set filetype=dotenv.sh
 autocmd BufNewFile,BufRead *.tl set filetype=teal
 autocmd BufNewFile,BufRead *.nix set filetype=nix
 autocmd BufNewFile,BufRead *.tmux set filetype=tmux.sh
+autocmd BufNewFile,BufRead *.conf set filetype=sh
+autocmd BufNewFile,BufRead .tmux.conf set filetype=tmux.sh
 autocmd BufNewFile,BufRead package.json set filetype=packageJson.json
 augroup END
 ]], true)
