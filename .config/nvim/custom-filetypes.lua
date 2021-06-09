50a9e01b033a9cf0c599927a180c33f1d2f80ad4
kghugo <hugosum.dev@protonmail.com>
2021-06-09 23:48:42 +0800

    Add type for purescript

--- .config/nvim/custom-filetypes.lua
+++ .config/nvim/custom-filetypes.lua
@@ -6,6 +6,7 @@ autocmd BufNewFile,BufRead *.tl set filetype=teal
 autocmd BufNewFile,BufRead *.nix set filetype=nix
 autocmd BufNewFile,BufRead *.tmux set filetype=tmux.sh
 autocmd BufNewFile,BufRead *.conf set filetype=sh
+autocmd BufNewFile,BufRead *.purs set filetype=purescript
 autocmd BufNewFile,BufRead .tmux.conf set filetype=tmux.sh
 autocmd BufNewFile,BufRead package.json set filetype=packageJson.json
 augroup END
