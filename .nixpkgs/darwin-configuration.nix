3d790f2a1ccd6f7a8f21cab411e12a56853c8d51
kghugo <hugosum.dev@protonmail.com>
2021-06-20 17:05:25 +0800

    Add elixir

--- .nixpkgs/darwin-configuration.nix
+++ .nixpkgs/darwin-configuration.nix
@@ -9,10 +9,14 @@
     }))
   ];
 
+  /* nixpkgs.config = { */
+      /* allowUnsupportedSystem = true; */
+  /* }; */
+
   # time.timeZone = Hongkong;
   environment.systemPackages = [
     pkgs.nixfmt
-    pkgs.alacritty
+    /* pkgs.alacritty */
     pkgs.tmux
     pkgs.git
     pkgs.go
@@ -34,14 +38,18 @@
     pkgs.cachix
     pkgs.exa
     pkgs.pandoc
-    /* For using react-native */
-	pkgs.watchman
-    /* pkgs.neovim-nightly */
+    # For using react-native
+    pkgs.watchman
+    # pkgs.neovim-nightly
     pkgs.rnix-lsp
-	pkgs.ccls
-	pkgs.ktlint
+    pkgs.ccls
+    pkgs.ktlint
     pkgs.cocoapods
     pkgs.watchexec
+    pkgs.haskell-language-server
+    pkgs.nix-direnv
+    pkgs.coreutils
+    pkgs.elixir
   ];
 
   # use nix-community cache, run once only
@@ -62,40 +70,44 @@
     "$HOME/Library/Python/3.9/bin"
     "$HOME/.npm-global/bin"
     "$HOME/.self-built/bin"
-	"$HOME/.local/bin"
+    "$HOME/.local/bin"
   ];
   environment.interactiveShellInit = ''
-                    alias find="fd"
-                    alias grep="rg"
-                    alias vi="nvim"
-                    alias vim="nvim"
-                    alias vimdiff="nvim -d"
-                    alias ls="exa --icons"
-                    # use gcc10 instead of appleclang
-                    alias gcc='gcc-11'
-                    alias cc='gcc-11'
-                    alias g++='g++-11'
-                    alias c++='c++-11'
-                    # add pip3 alias
-                    alias pip="pip3.9"
-                    # fzf with preview
-                    alias pfzf="fzf --preview 'bat --style=numbers --color=always --line-range :500 {}'"
-                    # Load tmuxp config
-                    alias start="sh ~/.tmuxp/start.sh"
+                alias find="fd"
+                alias grep="rg"
+                alias vi="nvim"
+                alias vim="nvim"
+                alias vimdiff="nvim -d"
+                alias ls="exa --icons"
+                # use gcc10 instead of appleclang
+                alias gcc='gcc-11'
+                alias cc='gcc-11'
+                alias g++='g++-11'
+                alias c++='c++-11'
+                # add pip3 alias
+                alias pip="pip3.9"
+                # fzf with preview
+                alias pfzf="fzf --preview 'bat --style=numbers --color=always --line-range :500 {}'"
+                # Load tmuxp config
+                alias start="sh ~/.tmuxp/start.sh"
 
-        			alias dotfiles='/run/current-system/sw/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
+    			alias dotfiles='/run/current-system/sw/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
 
-            		# Change cmake compiler
-                	export CC=$(which gcc-11)
-                	export CXX=$(which g++-11)
+        		# Change cmake compiler
+            	export CC=$(which gcc-11)
+            	export CXX=$(which g++-11)
 
-            		alias luamake=/Users/hugosum/.config/standalone/lua-language-server/3rd/luamake/luamake
-                  '';
+        		alias luamake=/Users/hugosum/.config/standalone/lua-language-server/3rd/luamake/luamake
+                eval "$(direnv hook zsh)"
+              '';
 
   fonts = {
     enableFontDir = true;
     fonts = with pkgs; [ nerdfonts ];
   };
+
+  # Auto upgrade nix package and the daemon service.
+  services.nix-daemon.enable = true;
 
   programs.zsh = {
     enable = true;
@@ -106,6 +118,19 @@
     # promptInit = "prompt off";
     promptInit = "";
   };
+
+  # programs.fzf = {
+  # enable = true;
+  # enableBashIntegration = true;
+  # enableZshIntegration = true;
+  # };
+
+  # programs.direnv = {
+  # enable = true;
+  # enableNixDirenvIntegration = true;
+  # # enableBashIntegration = true;
+  # enableZshIntegration = true;
+  # };
 
   #programs.ssh.knownHosts = [
 
@@ -144,7 +169,7 @@
     useDaemon = true;
     binaryCaches = [ "https://cache.nixos.org" ];
     trustedBinaryCaches = [ "https://cache.nixos.org" ];
-    buildCores = 2;
+    buildCores = 0;
     gc.automatic = true;
   };
 
