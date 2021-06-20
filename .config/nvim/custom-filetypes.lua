88920c07610efb4e3bcdc1d53ebf17e0cc1c4703
kghugo <hugosum.dev@protonmail.com>
2021-06-20 17:12:21 +0800

    Add elixir filetype

--- .config/nvim/custom-filetypes.lua
+++ .config/nvim/custom-filetypes.lua
@@ -1,8 +1,10 @@
-vim.api.nvim_exec([[
-augroup envFiletype
+vim.api.nvim_exec(
+	[[
+augroup EnvFiletype
 autocmd!
 autocmd BufNewFile,BufRead *.env set filetype=dotenv.sh
 autocmd BufNewFile,BufRead *.tl set filetype=teal
+autocmd BufNewFile,BufRead *.exs set filetype=elixir
 autocmd BufNewFile,BufRead *.nix set filetype=nix
 autocmd BufNewFile,BufRead *.tmux set filetype=tmux.sh
 autocmd BufNewFile,BufRead *.conf set filetype=sh
@@ -10,4 +12,16 @@ autocmd BufNewFile,BufRead *.purs set filetype=purescript
 autocmd BufNewFile,BufRead .tmux.conf set filetype=tmux.sh
 autocmd BufNewFile,BufRead package.json set filetype=packageJson.json
 augroup END
-]], true)
+]],
+	true
+)
+-- Set commentstring for fennel
+vim.api.nvim_exec(
+	[[
+augroup CommentString
+    autocmd!
+	autocmd BufNewFile,BufRead *.fnl set cms=;;%s
+augroup END
+]],
+	true
+)
