56a5f5d118010d3719329747b7617a667849f6a1
kghugo <hugosum.dev@protonmail.com>
2021-06-24 10:30:36 +0800

    Use lua keymapping for hop

--- .config/nvim/plugins/hop.lua
+++ .config/nvim/plugins/hop.lua
@@ -1,5 +1,6 @@
 local function init(paq)
-	paq({ "phaazon/hop.nvim" })
+	paq({ "winston0410/hop.nvim" })
+    -- paq({ "phaazon/hop.nvim" })
 	require("hop").setup({})
 	vim.cmd([[ nmap <Leader><Leader> <cmd>HopChar1<cr>]])
 	vim.cmd([[ vmap <Leader><Leader> <cmd>HopChar1<cr>]])
@@ -9,6 +10,8 @@ local function init(paq)
     local supported_modes = { "n", "v", "o"}
 
     for _, mode in ipairs(supported_modes) do
+        vim.api.nvim_set_keymap(mode, "<Leader><Leader>", "<cmd>HopChar1<cr>", { noremap = true, silent = true })
+        vim.api.nvim_set_keymap(mode, "<Leader>w", "<cmd>HopWord<cr>", { noremap = true, silent = true })
         -- vim.api.nvim_set_keymap(mode, "t", "<cmd>HopChar1AC<cr>", { noremap = true, silent = true })
         -- vim.api.nvim_set_keymap(mode, "T", "<cmd>HopChar1BC<cr>", { noremap = true, silent = true })
     end
