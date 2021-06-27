999475265da49d7dd8d1e5f6eff119b8b1eb1d04
kghugo <hugosum.dev@protonmail.com>
2021-06-27 10:17:21 +0800

    Migrate package installation to nix

--- .config/go/install.sh
+++ .config/go/install.sh
@@ -1,6 +1,5 @@
 PACKAGES=$(
 	cat <<-END
-		golang.org/x/tools/gopls@latest
 	END
 )
 
@@ -8,10 +7,8 @@ USER_PACKAGES=$(
 	cat <<-END
 		github.com/jessfraz/dockfmt
 		github.com/lighttiger2505/sqls
-		golang.org/x/tools/cmd/goimports
-		golang.org/x/lint/golint
 	END
 )
 
-GO111MODULE=on go get $PACKAGES
+# GO111MODULE=on go get $PACKAGES
 go get $USER_PACKAGES
